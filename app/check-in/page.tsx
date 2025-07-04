e1d3c70a446f89d136c6c78215227b048e19c5b4
saboiii <saba.x.azad@gmail.com>
2025-07-04 23:26:54 +0800

    added suspense boundary

--- app/check-in/page.tsx
+++ app/check-in/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useSearchParams } from "next/navigation";
 import { GoChevronDown } from "react-icons/go";
 import { BiKey } from "react-icons/bi";
@@ -43,67 +43,74 @@ export default function CheckInPage() {
   };
 
   return (
-    <div className="flex flex-col w-full h-screen items-center justify-center px-12">
-      {message ? (
-        <p className="text-2xl text-center">{message}</p>
-      ) : (
-        <div className="flex w-full flex-col items-center justify-center gap-3 ">
-          <BiKey className="flex" size={64} />
-          <h2 className="flex font-homevideo font-bold">Check Into {room}</h2>
-          <p className="flex w-full md:w-1/4 text-center text-pretty text-xs px-8 mb-10">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos debitis officia, cumque culpa.
-          </p>
-          <form
-            onSubmit={handleSubmit}
-            className="flex flex-col gap-3 w-full md:w-2/5"
-          >
-            <div className="flex relative w-full">
-              <select
-                value={district}
-                onChange={(e) => setDistrict(e.target.value)}
-                className="formSelect font-homevideo"
-                required
-              >
-                <option value="" disabled>
-                  Select District
-                </option>
-                {districts.map((d) => (
-                  <option key={d} value={d}>
-                    {d}
-                  </option>
-                ))}
-              </select>
-              <GoChevronDown className="absolute top-4 right-4" />
-            </div>
-
-            <div className="flex relative w-full">
-              <select
-                value={subOG}
-                onChange={(e) => setSubOG(e.target.value)}
-                className="formSelect font-homevideo"
-                required
-              >
-                <option value="" disabled>
-                  Select SubOG
-                </option>
-                {subOGs.map((s) => (
-                  <option key={s} value={s}>
-                    {s}
-                  </option>
-                ))}
-              </select>
-              <GoChevronDown className="absolute top-4 right-4" />
-            </div>
-            <button
-              type="submit"
-              className="py-3 bg-white text-semibold text-black font-homevideo rounded-lg mt-4"
-              disabled={loading}
+    <Suspense fallback={
+      <div className="flex flex-col w-full h-screen items-center justify-center px-12">
+        <p className="uppercase text-xs font-homevideo">Loading...</p>
+      </div>
+    }>
+      <div className="flex flex-col w-full h-screen items-center justify-center px-12">
+        {message ? (
+          <p className="text-2xl text-center">{message}</p>
+        ) : (
+          <div className="flex w-full flex-col items-center justify-center gap-3 ">
+            <BiKey className="flex" size={64} />
+            <h2 className="flex font-homevideo font-bold">Check Into {room}</h2>
+            <p className="flex w-full md:w-1/4 text-center text-pretty text-xs px-8 mb-10">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam eos debitis officia, cumque culpa.
+            </p>
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-3 w-full md:w-2/5"
             >
-              {loading ? "Checking in..." : "Check In"}
-            </button>
-          </form>
-        </div>
-      )}
-    </div>
+              <div className="flex relative w-full">
+                <select
+                  value={district}
+                  onChange={(e) => setDistrict(e.target.value)}
+                  className="formSelect font-homevideo"
+                  required
+                >
+                  <option value="" disabled>
+                    Select District
+                  </option>
+                  {districts.map((d) => (
+                    <option key={d} value={d}>
+                      {d}
+                    </option>
+                  ))}
+                </select>
+                <GoChevronDown className="absolute top-4 right-4" />
+              </div>
+
+              <div className="flex relative w-full">
+                <select
+                  value={subOG}
+                  onChange={(e) => setSubOG(e.target.value)}
+                  className="formSelect font-homevideo"
+                  required
+                >
+                  <option value="" disabled>
+                    Select SubOG
+                  </option>
+                  {subOGs.map((s) => (
+                    <option key={s} value={s}>
+                      {s}
+                    </option>
+                  ))}
+                </select>
+                <GoChevronDown className="absolute top-4 right-4" />
+              </div>
+              <button
+                type="submit"
+                className="py-3 bg-white text-semibold text-black font-homevideo rounded-lg mt-4"
+                disabled={loading}
+              >
+                {loading ? "Checking in..." : "Check In"}
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
+    </Suspense>
+
   );
 }
